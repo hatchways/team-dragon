56a19ffcd8e0539e0a0e3b32837d5ccf482b7830
Nicholas Chumney <nchumney@hotmail.com>
2020-09-14 19:45:02 -0400

    added auth controller and valiations

---
+++ server/config/passport.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const passportJwt = require('passport-jwt');
+const config = require('../config');
+const User = require('../models/User');
+
+const ExtractJwt = passportJwt.ExtractJwt;
+const JwtStrategy = passportJwt.Strategy;
+const opts = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: config.secret,
+}
+
+const jwtStrategy = new JwtStrategy(opts, (payload, done) => {
+  User.findById(payload.id)
+    .then(user => {
+      if(user) {
+        return done(null, user);
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      return done(null, false);
+    });
+});
+
+module.exports = jwtStrategy;
