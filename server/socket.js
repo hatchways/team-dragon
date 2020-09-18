973f81958285cd2adccdd55c67a628b7add6b2c2
Nicholas Chumney <nchumney@hotmail.com>
2020-09-18 16:08:35 -0400

    fixed some server problems and restored env.example

--- server/socket.js
+++ server/socket.js
@@ -1,68 +1,56 @@
-
 let ioExport;
+
 exports.socket = (server) => {
   const io = require("socket.io")(server);
   ioExport = io;
+  let message_id = 0;
+  const roomDetails = {};
 
-  // let message_id = 0;
-  // const roomDetails = {};
+  io.on("connection", socket => {
+    socket.on("join", (room, fn) => {
+      // join a room
+      socket.join(room);
+      if(roomDetails[room] == undefined) {
+        // create room details if does not exist
+        roomDetails[room] = {
+          history: [],
+          users: [],
+        }
+      }
 
-  io.on("connection", (socket) => {
-    console.log("user connected");
+      // assign user a name and store user details
+      const assignedName = "guest-" + socket.id.substr(0, 5);
+      const user = { id: socket.id, name: assignedName };
+      roomDetails[room].users.push(user);
 
-    // socketExport = socket;
-    // console.log("Match socket connected");
+      // return assigned name and chat history
+      fn({ 
+        name: assignedName,
+        history: roomDetails[room].history,
+      });
+    });
 
-    // socket.on("joinmatch", (room) => {
-    //   socket.join("match1");
-    //   console.log(room);
-    //   io.to("match1").emit("joinedmatch", "New user joined");
-    // });
+    socket.on("message", (room, { sender, message }) => {
+      // save message into history and update all clients
+      roomDetails[room].history.unshift({ 
+        id: message_id++,
+        sender,
+        message,
+      });
 
-    // socket.on("join", (room, fn) => {
-    //   // join a room
-    //   socket.join(room);
-    //   if (roomDetails[room] == undefined) {
-    //     // create room details if does not exist
-    //     roomDetails[room] = {
-    //       history: [],
-    //       users: [],
-    //     };
-    //   }
+      io.to(room).emit('message', roomDetails[room].history);
+    });
 
-    //   // assign user a name and store user details
-    //   const assignedName = "guest-" + socket.id.substr(0, 5);
-    //   const user = { id: socket.id, name: assignedName };
-    //   roomDetails[room].users.push(user);
+    socket.on("leave", (room) => {
+      // update users currently in the chat
+      roomDetails[room].users = roomDetails[room].users.filter( 
+        user => user.id !== socket.id
+      );
+    })
 
-    //   // return assigned name and chat history
-    //   fn({
-    //     name: assignedName,
-    //     history: roomDetails[room].history,
-    //   });
-    // });
-
-    // socket.on("message", (room, { sender, message }) => {
-    //   // save message into history and update all clients
-    //   roomDetails[room].history.unshift({
-    //     id: message_id++,
-    //     sender,
-    //     message,
-    //   });
-
-    //   io.to(room).emit("message", roomDetails[room].history);
-    // });
-
-    // socket.on("leave", (room) => {
-    //   // update users currently in the chat
-    //   roomDetails[room].users = roomDetails[room].users.filter(
-    //     (user) => user.id !== socket.id
-    //   );
-    // });
-
-    // socket.on("disconnect", () => {
-    //   console.log("user disconnected");
-    // });
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    })
   });
 };
 
@@ -70,5 +58,6 @@ exports.getIO = () => {
   if (!ioExport) {
     console.log("io not initialized!");
   }
+  
   return ioExport;
 };
