6a0b5908cbdbaebe902228465f9579d507a17262
Karl Secen <karl.secen@gmail.com>
2020-09-22 10:59:32 -0400

    start over button

---
+++ client/src/components/GameSetup.js
@@ -0,0 +1,20 @@
+import React, {useEffect, useState} from 'react'
+
+const GameSetup = () => {
+    const [isHost, setisHost] = useState(false)
+
+    useEffect(() => {
+        //Request
+   
+
+    }, [])
+
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default GameSetup
