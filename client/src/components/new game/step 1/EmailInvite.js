526beb793934d4fb1868465c1ea2882deccf1162
Karl Secen <karl.secen@gmail.com>
2020-09-22 14:10:46 -0400

    package fix

--- client/src/components/new game/step 1/EmailInvite.js
+++ client/src/components/new game/step 1/EmailInvite.js
@@ -3,6 +3,7 @@ import { useEmails } from "../../../DataContext";
 import { useState } from "react";
 import { Typography, TextField, Button } from "@material-ui/core";
 import { makeStyles, createStyles } from "@material-ui/core/styles";
+import isEmail from "validator/lib/isEmail";
 
 const useStyles = makeStyles((theme) =>
   createStyles({
@@ -18,13 +19,29 @@ const EmailInvite = () => {
   const [emails, setEmails] = emailsContext;
 
   const [emailForm, setEmailForm] = useState("");
+  const [validate, setValidate] = useState({ error: false, message: "" });
 
   const classes = useStyles();
 
-  const handleSubmit = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
     // Prevents empty tag submission
-    if (emailForm === "") return; //needs additional logic to verify if valid email address
+    if (emailForm === "") {
+      setValidate({ error: false, message: "" });
+      return; //needs additional logic to verify if valid email address
+    }
 
+    if (emails.includes(emailForm)) {
+      setValidate({ error: true, message: "User already invited" });
+      setEmailForm("");
+      return;
+    }
+
+    if (!isEmail(emailForm)) {
+      setValidate({ error: true, message: "Not a valid email address" });
+      return;
+    }
+    setValidate({ error: false, message: "" });
     setEmails([...emails, emailForm]);
     setEmailForm("");
   };
@@ -36,20 +53,25 @@ const EmailInvite = () => {
   return (
     <>
       <Typography variant="h3">Invite friends via email</Typography>
-      <TextField
-        label="Email Address"
-        variant="outlined"
-        value={emailForm}
-        onChange={(e) => setEmailForm(e.target.value)}
-      />
+      <form className={classes.form} onSubmit={handleSubmit}>
+        <TextField
+          label="Email Address"
+          variant="outlined"
+          value={emailForm}
+          error={validate.error === true}
+          helperText={validate.message}
+          onChange={(e) => setEmailForm(e.target.value)}
+        />
 
-      <Button
-        variant="contained"
-        className={classes.invite}
-        onClick={handleSubmit}
-      >
-        Send invite
-      </Button>
+        <Button
+          variant="contained"
+          className={classes.invite}
+          // onClick={handleSubmit}
+          type="submit"
+        >
+          Send invite
+        </Button>
+      </form>
       {emails.length > 0 && (
         <div>
           <ul>{displayEmails}</ul>
