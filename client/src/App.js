cc45cf97138bec8ef5860e78c7988c991505caf4
Nicholas Chumney <nicholas.chumney@outlook.com>
2020-10-06 12:19:08 -0400

    moved Providers to index.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,5 @@
 import React from "react";
-import { MuiThemeProvider } from "@material-ui/core";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import { DataProvider } from "./contexts/DataContext";
-import { GameProvider } from "./contexts/GameContext";
-import { UserProvider } from "./contexts/UserContext";
-import { theme } from "./themes/theme";
 import NavBar from "./components/NavBar";
 import PrivateRoute from "./components/PrivateRoute";
 import Landing from "./pages/Landing";
@@ -15,28 +10,16 @@ import ProfileSettings from "./pages/ProfileSettings";
 
 const App = () => {
   return (
-    <DataProvider>
-      <GameProvider>
-        <UserProvider>
-          <MuiThemeProvider theme={theme}>
-            <BrowserRouter>
-              <NavBar />
-              <Switch>
-                <Route exact path="/register" component={Register} />
-                <Route exact path="/login" component={Login} />
-                <PrivateRoute
-                  exact
-                  path="/edit-profile"
-                  component={ProfileSettings}
-                />
-                <PrivateRoute exact path="/:id" component={GameSetup} />
-                <Route exact path="/" component={Landing} />
-              </Switch>
-            </BrowserRouter>
-          </MuiThemeProvider>
-        </UserProvider>
-      </GameProvider>
-    </DataProvider>
+    <BrowserRouter>
+      <NavBar />
+      <Switch>
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
+        <PrivateRoute exact path="/edit-profile" component={ProfileSettings} />
+        <PrivateRoute exact path="/:id" component={GameSetup} />
+        <Route exact path="/" component={Landing} />
+      </Switch>
+    </BrowserRouter>
   );
 };
 
